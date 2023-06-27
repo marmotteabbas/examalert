7f8a5b9d6c6ad106228fcc41a1168f97218a985e
root <root@moodle-test.grenet.fr>
2023-06-27 16:11:53 +0200

    rerefix because I did a mistake

--- lib.php
+++ lib.php
@@ -3,7 +3,7 @@
 function local_examalert_extend_navigation(global_navigation $navigation)
 {
     global $PAGE, $DB, $COURSE;
-    
+
     $myconfig = get_config('local_examalert');
 
 
@@ -40,8 +40,20 @@ function local_examalert_extend_navigation(global_navigation $navigation)
             );
             $dateend->setTime(23, 59);
         } else {
+
+            $sql = "SELECT q.timeopen, q.timeclose FROM course_modules cm 
+                INNER JOIN quiz q ON cm.instance = q.id 
+                WHERE cm.id = :id";
+
+            if (!isset($_GET['update'])) {
+              $thing_id = $_GET['cmid'];
+            } else {
+                $thing_id = $_GET['update'];
+            }
+            $dates = $DB->get_record_sql($sql, array("id" => $thing_id));
+
             // Begin date
-            $db = date('Y/m/d/H/i', 962110904);
+            $db = date('Y/m/d/H/i', $dates->timeopen);
             $datebegin = date_create();
 
             $datebegin->setDate(explode(
@@ -54,7 +66,7 @@ function local_examalert_extend_navigation(global_navigation $navigation)
 
             // End date
             $dateend = date_create();
-            $df = date('Y/m/d/H/i', 32519019704);
+            $df = date('Y/m/d/H/i', $dates->timeclose);
 
             $dateend->setDate(explode(
                 "/", $df)[0],
@@ -70,13 +82,14 @@ function local_examalert_extend_navigation(global_navigation $navigation)
 
         if ($myconfig->active == 1 && ((($datebegin < $currentdate) || ($datebegin->getTimestamp() == 0 && $dateend->getTimestamp() !== 0)) && (($dateend > $currentdate) || ($dateend->getTimestamp() == 0) && $datebegin->getTimestamp() !== 0))) {
             $PAGE->requires->css('/local/examalert/style.css', true);
+            
             $message = "Attention, ce message vous est adressé car nous sommes en période d'examen ou dans les horaires d'ouverture de votre test.<br /> 
             Si des étudiants sont en train de passer votre test, il est important de noter que modifier les paramètres de ce test à ce stade peut avoir<br /> 
             des conséquences néfastes sur l'expérience des étudiants et les résultats obtenus. <br />
             Cela s'applique particulièrement à des éléments tels que la durée du test et les modes de notation. <br />
             Les dérogations spécifiques accordées aux étudiants ne sont pas concernées par cette restriction et peuvent être réalisées pendant le test, 
             <br />le cas échéant. Nous vous demandons donc de bien prendre en compte ces aspects avant de procéder à toute modification du test. <br />
-            Si vous avez des doutes ou des questions, n'hésitez pas à contacter sos-dapi@univ-grenoble-alpes.fr. <br />
+            Si vous avez des doutes ou des questions, n'hésitez pas à contacter sos-dapi@univ-grenoble-alpes.fr. <br /><br />
             
             <a href='javascript:void(0);' ONCLICK='document.getElementById(\"warning_message\").style.display = \"none\";document.getElementById(\"mask\").style.display = \"none\"'>Je souhaite malgré tout accéder au paramétrage du test</a>
             <br />
